db100f244ca3e76b49d16d0e99e6b04956053dca
AjeAyoub <ajeddabayoub.official@gmail.com>
2024-01-13 02:40:52 -0800

    BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 class BaseModel:
     """BaseModel class for AirBnB project."""
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                else:
+                elif key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -35,15 +35,25 @@ class BaseModel:
         return obj_dict
 
 if __name__ == "__main__":
-    # Example usage in your test_base_model.py or for debugging
+    # Example usage in your tests or for debugging
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
